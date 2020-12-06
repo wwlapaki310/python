4331fdd01d6c51d92ba4496a5232049ebc097dd4
satoru akita <wwlap24@gmail.com>
2020-12-06 21:36:05 +0900

    Create auto3.py

---
+++ auto/auto3.py
@@ -0,0 +1,38 @@
+import pyautogui
+print('Ctrl+Cを押すと終了します')
+try:
+    while True:
+        print("start")
+        pyautogui.click(x=300,y=800)
+        pyautogui.press("up")
+        pyautogui.press("enter")
+        pyautogui.click(x=900,y=800)
+        pyautogui.press("up")
+        pyautogui.press("enter")
+        pyautogui.click(x=1600,y=800)
+        pyautogui.press("up")
+        pyautogui.press("enter")
+        print("during")
+        pyautogui.sleep(20)
+        pyautogui.click(x=300,y=800)
+        pyautogui.hotkey("ctrlleft","c")
+        pyautogui.click(x=900,y=800)
+        pyautogui.hotkey("ctrlleft","c")
+        pyautogui.click(x=1200,y=800)
+        pyautogui.hotkey("ctrlleft","c")
+        print("end")
+        pyautogui.sleep(10)
+except KeyboardInterrupt:
+    print('\n終了')
+
+# x300 y730
+# x900 y800
+#x1600 y800
+
+
+#x200 y400
+#x200 y800
+#x800 y400
+#x800 y800
+#1400 y400
+#x1400 y800
